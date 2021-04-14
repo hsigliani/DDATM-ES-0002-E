a7378e986d13176956c37acd8af6e6ea94b72e96
hernan.sigliani <hernan.sigliani@gmail.com>
2021-04-14 08:50:36 -0300

    Adds comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 def complete_array(arr):
     _max = 0
+    # just validates the input and searches for the max number. Then creates a list from 1 to that number.
     for x in arr:
         if type(x) == int and x >= 0:
             _max = max(_max, x)
@@ -16,5 +17,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('usage: python main.py <integer array> # without spaces between eg. [1,12,4]')
     else:
-        arr = sys.argv[1][1:len(sys.argv[1])-1].split(',')
-        print(complete_array([int(i) for i in arr]))
+        i_arr = sys.argv[1][1:len(sys.argv[1])-1].split(',')
+        print(complete_array([int(i) for i in i_arr]))
